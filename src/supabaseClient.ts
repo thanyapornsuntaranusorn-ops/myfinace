ed7f60f67837727ef3151cc1536a839ae0e21579
jcjcape <thanyapornsuntaranusorn@gmail.com>
2026-09-16 21:47:08 +0700

    fix supabase url path

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // เปลี่ยนข้อความใน '...' ให้เป็นค่าจริงจาก Supabase
-const supabaseUrl = 'https://mhxatmvuicnpjhqxdngc.supabase.co/rest/v1/'; 
+const supabaseUrl = 'https://mhxatmvuicnpjhqxdngc.supabase.co'; 
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oeGF0bXZ1aWNucGpocXhkbmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NDg0MzIsImV4cCI6MjEwNTEyNDQzMn0.wwWULGQlfhgeUkXMpZtC284q0S8F0wDz1jkDIoduk8A'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
